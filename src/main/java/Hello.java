e52a95a841c7afe92a37f17d6c56d10f65eade47
Luciehtle <Lushee@hotmail.fr>
2021-05-26 09:20:14 +0200

    Hello Java improved

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.printIn("Hello from the potager");
+		System.out.printIn("Hello from the potager of Lucie and Noe");
 	}
 }
